d8e2a0e4f73abcba642cba70f5ebe13f1595eaba
Vladimir Yakunin <vyakunin@gmail.com>
2018-03-18 14:18:07 -0700

    draft voice recognition

--- android/app/src/main/java/io/peggyjo/peggymeter/MainActivity.java
+++ android/app/src/main/java/io/peggyjo/peggymeter/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
