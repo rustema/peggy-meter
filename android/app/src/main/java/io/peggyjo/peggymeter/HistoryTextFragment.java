170d544b5c5f79fb27b86a71fe2710de69997da1
JamieABall <jaball@ucsc.edu>
2018-04-27 19:48:34 -0700

    Note edit functionality first half.
    Signed-off-by: JamieABall <jaball@ucsc.edu>

--- android/app/src/main/java/io/peggyjo/peggymeter/HistoryTextFragment.java
+++ android/app/src/main/java/io/peggyjo/peggymeter/HistoryTextFragment.java
@@ -1,12 +1,19 @@
 package io.peggyjo.peggymeter;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
+
+import com.google.firebase.database.DatabaseReference;
 
 import java.util.List;
 
@@ -31,6 +38,37 @@ public class HistoryTextFragment extends Fragment implements MoodListener {
         dataController.addMoodListener(this);
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_history_text, container, false);
+        ListView table = view.findViewById(R.id.LOG_ENTRIES_CONTAINER);
+        table.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                LogEntry selectedEntry = (LogEntry)table.getItemAtPosition(i); //used instead of getSelectedItem because getSelectedItem causes a null pointer exception
+                EditText note = new EditText(getContext());
+
+                if(!selectedEntry.getComment().isEmpty()) {
+                    note.setText(selectedEntry.getComment());
+                }
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+                if(!selectedEntry.getComment().isEmpty()) {
+                    builder.setTitle("Edit note");
+                } else {
+                    builder.setTitle("Add a note");
+                }
+                builder.setView(note);
+                AlertDialog editor = builder.create();
+                editor.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialogInterface) {
+                        LogEntry editedEntry = new LogEntry(selectedEntry.getTime(), selectedEntry.getMood_level(), note.getText().toString());
+                        dataController.getMoodAdapter().addEntry(editedEntry); //to be replaced
+
+                    }
+                });
+                editor.show();
+                return true;
+            }
+        });
         renderLog(view, dataController.getMoodAdapter().getLogs());
         return view;
     }
