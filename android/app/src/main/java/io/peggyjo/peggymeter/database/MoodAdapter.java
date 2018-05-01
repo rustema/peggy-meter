e54640a2abba07d470371866a12c51fae30296c1
JamieABall <jaball@ucsc.edu>
2018-04-30 18:19:30 -0700

    Resolve merge conflict
    Signed-off-by: JamieABall <jaball@ucsc.edu>

--- android/app/src/main/java/io/peggyjo/peggymeter/database/MoodAdapter.java
+++ android/app/src/main/java/io/peggyjo/peggymeter/database/MoodAdapter.java
@@ -12,10 +12,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.Iterator;
 
 
 import io.peggyjo.peggymeter.model.LogEntry;
